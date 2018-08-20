50ccebd0c1c135acba222e5b65f73e8954b66be1
mikeyrayvon <mikey@interglobal.vision>
2018-08-20 14:11:49 -0500

    scaffold footer

--- footer.php
+++ footer.php
@@ -1,4 +1,41 @@
+
   <footer id="footer">
+  <?php
+    $home_page = get_page_by_path('home');
+
+    if(!empty($home_page)) {
+      $email = get_post_meta($home_page->ID, '_igv_contact_email', true);
+      $pdf = get_post_meta($home_page->ID, '_igv_portfolio_pdf', true);
+  ?>
+    <div class="grid-row justify-end">
+    <?php
+      if (!empty($email)) {
+    ?>
+      <div class="grid-item">
+        <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+      </div>
+    <?php
+      }
+
+      if (!empty($email) && !empty($pdf)) {
+    ?>
+      <div class="grid-item"><span>/</span></div>
+    <?php
+      }
+
+      if (!empty($pdf)) {
+    ?>
+      <div class="grid-item">
+        <a href="<?php echo $pdf; ?>">Descargar PDF</a>
+      </div>
+    <?php
+      }
+    ?>
+    ?>
+    </div>
+  <?
+    }
+  ?>
   </footer>
 
 </section>
