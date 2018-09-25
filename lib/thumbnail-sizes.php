0845efe92186b39d739dea6b1e96ddd51d86ab4f
mikeyrayvon <mikey@interglobal.vision>
2018-09-25 14:55:58 -0500

    add image sizes

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,5 +8,10 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'gallery', 1200, 9999, false );
+  add_image_size( 'mobile', 320, 9999, false );
+  add_image_size( 'tablet', 768, 9999, false );
+  add_image_size( 'tablet-pro', 1024, 9999, false );
+  add_image_size( 'laptop-small', 1280, 9999, false );
+  add_image_size( 'laptop-large', 1440, 9999, false );
+  add_image_size( 'monitor', 1920, 9999, false );
 }
